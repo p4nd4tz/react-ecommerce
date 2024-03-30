249db2db62fcc354f784ad74c763c23aaccbdccd
p4nd4tz <abhisheksharma521020@gmail.com>
2024-03-30 20:32:05 +0530

    login and signup front

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import ProductList from './features/product-list/ProductList';
 import Home from './pages/Home'
+import LoginPage from './pages/LoginPage'
+import SignupPage from './pages/SignupPage';
 
 function App() {
   return (
